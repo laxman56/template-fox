68043227fdde629c6183fdaa2514ceacfa91d0a9
lkhanal56 <lkhanala56@gmail.com>
2015-12-29 23:28:18 +0545

    upto counter completed

--- js/Counter.js
+++ js/Counter.js
@@ -1,40 +1,41 @@
 function Counter(){
 	this.element = document.getElementsByClassName("counter");
-	console.log(element);
-	that = this;
 	var maxValue = [];
 	var counter = 0;
 	var maxCount;
 	var intervalId;
-
+  that = this;
 
 	this.startCounter = function(){
 		var length = that.element.length;
+
 		for(var i = 0; i < length; i++){
 			maxValue.push(parseInt(that.element[i].innerHTML));
 		}
+
+    /*Finding Maximum value upto which counter has to be increased*/
 		maxCount = Math.max(...maxValue);
 
 		intervalId = setInterval(function(){
+
 			for(var j = 0; j < length; j++){
 				if(counter <= maxValue[j])
 					element[j].innerHTML = counter;
 			}
-			counter = counter+10;
-			// console.log(maxCount,counter);
+
+			counter = counter+30;
+
 			if( counter > maxCount )
 				clearInterval(intervalId);
-			console.log('exceded');
+				
 		},1);
 
 	}
 
 
-
-
-
 	window.addEventListener('scroll', function(){
 		var posY = window.scrollY;
+    
 		if(posY >= 519){
 			that.startCounter();
 		}
